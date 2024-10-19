76bc4d4191cc75454121dbaeca992c8caf801e08
mike911209 <mike911209@gmail.com>
2024-10-19 13:59:26 +0800

    feat: chat event

---
+++ server/chat/event.py
@@ -0,0 +1,68 @@
+from chat.eventType import EventType
+from datetime import datetime
+import json
+import uuid
+
+AGENT_COUNT = 8
+CHAT_PATH = "server/chat/chat_history.json"
+agentState = []
+
+chat_history = []
+
+def loadChatHistory():
+    global chat_history
+    try:
+        with open(CHAT_PATH, "r") as f:
+            chat_history = json.load(f)
+    except FileNotFoundError:
+        chat_history = []
+
+def initAgentState():
+    global agentState
+    for i in range(AGENT_COUNT):
+        agentState.append(False)
+
+def updateChatHistory(input, eventType):
+    # print("Event handler received input: " + input)
+    response = ""
+    if eventType == EventType.USER_INPUT:
+        print("Receive user input")
+        # response = agentManager(input, agentState)
+        response = input
+    elif eventType == EventType.AGENT_INPUT:
+        print("Receive agent input")
+        response = input
+
+    # update chat history
+    chat_history.append({
+        'speaker': 0 if eventType == EventType.USER_INPUT else 1,
+        'message': response,
+        'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        'id': str(uuid.uuid4())
+    })
+
+    # print("Chat history: ", chat_history)
+    with open(CHAT_PATH, "w") as f:
+        json.dump(chat_history, f, indent=4)
+
+def getChatHistory():
+    return chat_history
+
+def clearChatHistory():
+    global chat_history
+    chat_history = []
+    with open(CHAT_PATH, "w") as f:
+        json.dump(chat_history, f, indent=4)
+
+def toggleAgent(agent_id):
+    agentState[agent_id] = not agentState[agent_id]
+
+if __name__ == '__main__':
+    print(getChatHistory())
+    updateChatHistory("Hello", EventType.USER_INPUT)
+    updateChatHistory("Hi", EventType.AGENT_INPUT)
+    updateChatHistory("How are you?", EventType.USER_INPUT)
+    updateChatHistory("I am fine", EventType.AGENT_INPUT)
+    updateChatHistory("Goodbye", EventType.USER_INPUT)
+    updateChatHistory("Bye", EventType.AGENT_INPUT)
+    # print(getChatHistory())
---
+++ server/chat/eventType.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class EventType(Enum):
+    USER_INPUT = 1
+    AGENT_INPUT = 2
