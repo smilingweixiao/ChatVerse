a4123bcedcd673ed45f32e3abfdddaf902ae93ee
Smiley <y910906@gmail.com>
2024-10-20 07:43:22 +0800

    fix: leave but response bug

--- server/chat/event.py
+++ server/chat/event.py
@@ -37,12 +37,17 @@ def initAgentState(initial_state=None):
 
 def threadGetResponse():
     global lock, agentResponse, chat_history, agentResponseFlag
-    count = random.randint(0, 2)
+    # count = random.randint(0, 2)
     while True:
-        if count > 5:
-            agentResponseFlag = False
-            return
+        # if count > 5:
+        #     agentResponseFlag = False
+        #     return
         response, agent = get_agent_response(chat_history + agent_history, agentState)
+        if not agentState[agent]:
+            while True:
+                response, agent = get_agent_response(chat_history + agent_history, agentState)
+                if agentState[agent]:
+                    break
         # print("agent:", agent, ", response: ", response)
         
         with lock:
@@ -57,7 +62,7 @@ def threadGetResponse():
                 'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 'id': str(uuid.uuid4())
             })
-        count += 1
+        # count += 1
         
 
 def updateChatHistory(input, speaker, recording=False):
@@ -74,10 +79,10 @@ def updateChatHistory(input, speaker, recording=False):
             return True
         # print("Receive user input")
         response, agent = get_agent_response(chat_history, agentState)
-        if 'human' in agent:
+        if not agentState[agent] or 'human' in agent:
             while True:
                 response, agent = get_agent_response(chat_history, agentState)
-                if 'human' not in agent:
+                if agentState[agent] and 'human' not in agent:
                     break
         chat_history.append({
                 'speaker': agent,
